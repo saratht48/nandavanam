6c26297e7152388a8b16415adce1bce3e233c36a
saratht48 <saratht199748@gmail.com>
2022-03-03 12:43:25 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="sliderbase">
+  <carousel :items-to-show="6" :itemsToScroll="1">
+    <slide class="bgcolor" v-for="slide in 10" :key="slide">
+      <div class="carousel__item">{{ slide }}</div>
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+  </div>
+</template>
+
+<script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.sliderbase{
+  width: 50%;
+  margin: 0 auto;
+}
+.bgcolor{
+  background-color: cadetblue;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,55 @@
+<template>
+  <splide :options="options">
+    <splide-slide>
+      <div class="sliderdata"></div>
+    </splide-slide>
+    <splide-slide>
+      <div class="sliderdata"></div>
+    </splide-slide>
+    <splide-slide>
+      <div class="sliderdata"></div>
+    </splide-slide>
+    <splide-slide>
+      <div class="sliderdata"></div>
+    </splide-slide>
+    <splide-slide>
+      <div class="sliderdata"></div>
+    </splide-slide>
+    <splide-slide>
+      <div class="sliderdata"></div>
+    </splide-slide>
+  </splide>
+</template>
+
+
+<script>
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+export default {
+  name: 'HelloWorld',
+  data() {
+      return {
+        options: {
+          rewind : true,
+          gap    : '1rem',
+          perPage: 5,
+        },
+      };
+    },
+  props: {
+    msg: String
+  },
+  components: {
+    Splide,
+    SplideSlide,
+  },
+}
+</script>
+
+<style scoped>
+.sliderdata{
+  width: 100%;
+  height: 250px;
+  background-color: red;
+}
+</style>
